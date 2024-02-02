36b8fdb0038e0566ee6679076eb1a55d8ebc325f
Buek <anoulack1900@gmail.com>
2024-02-02 16:40:34 +0700

    commit

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const http = require('http');
 const app = require('./src/routes/app')
 
-require('./src/database')
+require('./src/database/index').connect()
 
 const server = http.createServer(app);
+
 
 const{API_PORT}=process.env;
 const port = process.env.PORT || API_PORT
